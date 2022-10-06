aa892d0d751fd5a636c66fdf5df7359d93119f35
Taimoor-sh <106023544+Taimoor-sh@users.noreply.github.com>
2022-10-06 12:59:56 +0500

    Add files via upload

---
+++ program.java
@@ -0,0 +1,12 @@
+
+/**
+ * program
+ */
+ /* This is class */class New {
+  /* instance variable */int x ; 
+  /* main method / body */public static void main(String[] args) {
+    /* local variable */int y ; 
+    /* Obj is created object */New obj = new New();
+    /* print line */System.out.println("Hello new");
+  }
+ }  
